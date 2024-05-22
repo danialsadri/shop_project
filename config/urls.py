e1e614dcc19c8c9ff2fe10e466994c1bb26cf3c2
danial sadri <danielsadri01@gmail.com>
2024-05-22 16:45:35 +0330

    update

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -6,3 +8,11 @@ urlpatterns = [
     path('', include('home.urls', namespace='home')),
     path('shop/', include('shop.urls', namespace='shop')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.sites.AdminSite.site_header = 'پنل مدیریت'
+admin.sites.AdminSite.site_title = 'پنل مدیریت'
+admin.sites.AdminSite.index_title = 'پنل مدیریت'
