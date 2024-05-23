e9f4d5404cda11c6e0b1efb72b59c748132008b6
danial sadri <danielsadri01@gmail.com>
2024-05-23 22:45:17 +0330

    update

--- apps/utils/models.py
+++ apps/utils/models.py
@@ -12,7 +12,6 @@ def file_upload_to(instance, filename):
 
 
 class BaseModel(models.Model):
-    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, unique=True, verbose_name='آیدی')
     created = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
     updated = models.DateTimeField(auto_now=True, verbose_name='تاریخ ویرایش')
 
