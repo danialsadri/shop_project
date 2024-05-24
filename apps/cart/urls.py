8a395186031f560321f1d26eb14e168299f513f2
danial sadri <danielsadri01@gmail.com>
2024-05-24 18:06:34 +0330

    update

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 app_name = 'cart'
-urlpatterns = []
+urlpatterns = [
+    path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+]
--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,1 +1,22 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from apps.shop.models import ProductModel
+from .cart import Cart
+
+
+@require_POST
+def add_to_cart(request, product_id):
+    try:
+        cart = Cart(request)
+        product = ProductModel.get_object_or_404(id=product_id)
+        cart.add(product)
+        context = {
+            'item_count': len(cart),
+            'total_price': cart.get_total_price(),
+        }
+        return JsonResponse(context)
+    except:
+        context = {
+            {'error': 'invalid request'},
+        }
+        return JsonResponse(context)
