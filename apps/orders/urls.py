420814b7a116b25c01ef25a57792af238f61969d
danial sadri <danielsadri01@gmail.com>
2024-05-26 06:42:34 +0330

    update

--- apps/orders/urls.py
+++ apps/orders/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 
 app_name = 'orders'
-urlpatterns = []
+urlpatterns = [
+    path('verify-phone/', views.verify_phone, name='verify_phone'),
+    path('verify-code/', views.verify_code, name='verify_code'),
+]
--- apps/orders/views.py
+++ apps/orders/views.py
@@ -1,1 +1,50 @@
-from django.shortcuts import render
+import random
+from django.contrib import messages
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from apps.accounts.models import User
+from .forms import PhoneVerificationForm
+
+
+def verify_phone(request):
+    if request.user.is_authenticated:
+        return redirect('orders:order_create')
+    if request.method == 'POST':
+        form = PhoneVerificationForm(request.POST)
+        if form.is_valid():
+            phone = form.cleaned_data['phone']
+            if User.objects.filter(phone=phone).exists():
+                messages.error(request, 'this phone is already registered.')
+                return redirect('orders:verify_phone')
+            else:
+                tokens = {'token': ''.join(random.choices('0123456789', k=6))}
+                request.session['verification_code'] = tokens['token']
+                request.session['phone'] = phone
+                print(tokens)
+                # send_sms_with_template(phone, tokens, 'verify')
+                messages.error(request, 'verification code sent successfully.')
+                return redirect('orders:verify_code')
+    else:
+        form = PhoneVerificationForm()
+    return render(request, 'orders/verify_phone.html', {'form': form})
+
+
+def verify_code(request):
+    if request.method == 'POST':
+        code = request.POST.get('code')
+        if code:
+            verification_code = request.session['verification_code']
+            phone = request.session['phone']
+            if code == verification_code:
+                user = User.objects.create_user(phone=phone)
+                user.set_password('123456')
+                user.save()
+                # send sms
+                print(user)
+                login(request, user)
+                del request.session['verification_code']
+                del request.session['phone']
+                return redirect('orders:order_create')
+            else:
+                messages.error(request, 'Verification code is incorrect.')
+    return render(request, 'orders/verify_code.html')
